82a18f0740c463d1658be429efdafba6fb639e8c
AmirHosein KazemNejad <ub.maka@gmail.com>
2016-05-10 13:29:42 +0430

    Add logo

--- gosocialserver/static/js/network.js
+++ gosocialserver/static/js/network.js
@@ -1,6 +1,11 @@
 function like(postId, likeTagId, disLikeTagId) {
     $.get('/posts/' + postId + '/like', postId,
         function (data, status, xhr) {
+            if (data.length > 20) {
+                window.location.href = "/auth/login";
+                return;
+            }
+
             var splitData = data.split('|');
             document.getElementById(likeTagId).innerHTML = splitData[0];
             document.getElementById(disLikeTagId).innerHTML = splitData[1];
@@ -9,6 +14,11 @@ function like(postId, likeTagId, disLikeTagId) {
 function disLike(postId, disLikeTagId, likeTagId) {
     $.get('/posts/' + postId + '/dislike', postId,
         function (data, status, xhr) {
+            if (data.length > 20) {
+                window.location.href = "/auth/login";
+                return;
+            }
+
             var splitData = data.split('|');
             document.getElementById(disLikeTagId).innerHTML = splitData[0];
             document.getElementById(likeTagId).innerHTML = splitData[1];
